c809a934305bc8c31089c53a0ff2beff0784636a
Yehia <ahmedyehia611@gmail.com>
2025-01-11 00:53:02 +0200

    feat: entry point for the modular monolith app

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"docbooking/internal/modules/availability"
+	"docbooking/internal/modules/booking"
+	"docbooking/pkg/event"
+	"docbooking/pkg/logger"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// the modular monolith entry point used to initialize all the modules
+	eventBus := event.NewEventBus()
+	log := logger.NewLogger()
+
+	// initialize the availability module
+	availabilityModule := availability.NewAvailabilityModule(eventBus, log)
+	bookingModule := booking.NewBookModule(eventBus, log)
+
+	// initializing the server
+	mux := http.NewServeMux()
+	mux.Handle("/availabilities/", availabilityModule.Handler)
+	mux.Handle("/booking/", bookingModule.Handler)
+
+	// running the server
+	log.Infof("Server is running on port %s", "8080")
+	http.ListenAndServe(":8080", mux)
 }
