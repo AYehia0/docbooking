70a786982e9e5d050e3ab616d4b87b3207e96dda
Yehia <ahmedyehia611@gmail.com>
2025-01-24 22:24:07 +0200

    feat: centeralized event types for ease of communications

--- pkg/event/event.go
+++ pkg/event/event.go
@@ -1,6 +1,43 @@
 package event
 
+import (
+	"time"
+
+	"github.com/google/uuid"
+)
+
 type Event struct {
 	Name    string
 	Payload interface{}
+}
+
+type CreateSlotEvent struct {
+	DoctorID uuid.UUID
+	Slot     Slot
+}
+
+type CreateAppointmentEvent struct {
+	Appointment Appointment
+}
+
+// Shared domain types: don't know where to put them yet
+type Slot struct {
+	ID         uuid.UUID
+	StartTime  time.Time
+	EndTime    time.Time
+	IsReserved bool
+	DoctorID   uuid.UUID
+	DoctorName string
+	Cost       float64
+}
+
+type Appointment struct {
+	ID          uuid.UUID
+	SlotID      uuid.UUID
+	PatientID   uuid.UUID
+	DoctorID    uuid.UUID
+	StartTime   time.Time
+	EndTime     time.Time
+	PatientName string
+	ReservedAt  time.Time
 }
